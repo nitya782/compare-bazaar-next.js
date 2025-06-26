ffe4daf817110b29e7550ebe9f61cd1259b6ebd4
Nitya Sharma <nsharmacs430@gmail.com>
2025-06-26 15:23:30 +0530

    Quick sand added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,15 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Quicksand } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import WideDiv from "../components/WideDiv";
 import Footer from "../components/Footer";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const quicksand = Quicksand({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'], // Customize as needed
+  variable: '--font-quicksand',
+  display: 'swap',
 });
-
 
 export default function RootLayout({ children }) {
   return (
